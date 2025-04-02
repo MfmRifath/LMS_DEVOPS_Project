64cd0a0d6e3e7505c981a5089c43bdeb14384b6f
Rifath MFM <mmfmrifath@gmail.com>
2025-04-02 09:58:33 +0530

    axc

--- lms_backend/settings.py
+++ lms_backend/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -62,7 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+
     # ... other middleware classes ...
     
     # Add the allauth middleware here, typically after the session middleware
